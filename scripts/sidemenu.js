608ae8a59a9e8bd18ed8fafdfc1791143c904dd4
erielrj <eriel.rj@gmail.com>
2020-11-20 22:19:00 -0300

    Incrementado o evento click nos links da sidenav para disparar as função closeNav

--- scripts/sidemenu.js
+++ scripts/sidemenu.js
@@ -1,7 +1,21 @@
 function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
+  document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
+
+
+/* Colocando o evento click nos links da sidenav para disparar as função closeNav  */
+
+(function () {
+  const navLinks = document.querySelectorAll('nav > a');
+
+  for (link of navLinks) {
+    link.addEventListener('click', function () {
+      closeNav();
+    })
   }
-  
-  function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-  }
+
+})();
