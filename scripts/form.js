41dd599acadebad6e43570d0d278f3e56fcff6a4
erielrj <eriel.rj@gmail.com>
2020-12-03 21:33:50 -0300

    Dados do formulário são enviados para o whatsapp

---
+++ scripts/form.js
@@ -0,0 +1,83 @@
+const formInputs = document.querySelectorAll('input.form-control');
+
+
+function setErrorClasses(inputNames) {
+    const formInputs = document.querySelectorAll('.form-control');
+
+    console.log(inputNames);
+
+    if (inputNames.includes("nome")) {
+        formInputs[0].className += " input-error";
+        formInputs[0].placeholder = "Digite um nome!!!";
+    } else {
+        formInputs[0].className = "form-control";
+        formInputs[0].placeholder = "Nome";
+    }
+
+    if (inputNames.includes("email")) {
+        formInputs[1].className += " input-error";
+        formInputs[1].placeholder = "Digite um email!!!";
+    } else {
+        formInputs[1].className = "form-control";
+        formInputs[1].placeholder = "Email";
+    }
+
+    if (inputNames.includes("endereco")) {
+        formInputs[2].className += " input-error";
+        formInputs[2].placeholder = "Digite um endereco!!!";
+    } else {
+        formInputs[2].className = "form-control";
+        formInputs[2].placeholder = "Endereço";
+    }
+
+    if (inputNames.includes("telefone")) {
+        formInputs[3].className += " input-error";
+        formInputs[3].placeholder = "Digite um telefone!!!";
+    } else {
+        formInputs[3].className = "form-control";
+        formInputs[3].placeholder = "Telefone";
+    }
+
+    if (inputNames.includes("cep")) {
+        formInputs[4].className += " input-error";
+        formInputs[4].placeholder = "Digite um cep!!!";
+    } else {
+        formInputs[4].className = "form-control";
+        formInputs[4].placeholder = "CEP";
+    }
+}
+
+
+function checkErrors(inputsArray) {
+    const errors = new Array();
+
+    for (input of inputsArray) {
+        if (input.value == "") {
+            errors.push(input.name);
+        }
+    }
+
+    if (errors[0]) {
+        setErrorClasses(errors);
+    } else {
+        return true;
+    }
+
+}
+
+(function () {
+    const formButton = document.querySelector('#formButton');
+    formButton.addEventListener("click", function () {
+        const formInputs = document.querySelectorAll('input.form-control');
+        if(checkErrors(formInputs)){
+            const nome = formInputs[0].value;
+            const email = formInputs[1].value;
+            const endereco = formInputs[2].value;
+            const telefone = formInputs[3].value;
+            const cep = formInputs[4].value;
+
+            window.open('https://api.whatsapp.com/send?phone=5521997170603&text=' + encodeURIComponent(`Oi, encontrei vocês pelo site, meu nome é: ${nome}. Meus dados são email: ${email}, enedereço: ${endereco}, telefone: ${telefone}, cep: ${cep}.`));
+        };
+    })
+}
+)();
